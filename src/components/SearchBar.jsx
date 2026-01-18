25b17304707339c2a7941b46223f6fca2a4fadd4
Jubril <jubfeb20@gmail.com>
2026-01-18 08:01:47 +0100

    Adding the search bar

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,5 +1,21 @@
+import UnitItem from "./UnitItem";
+
 function SearchBar() {
-  return <div>SearchBar</div>;
+  return (
+    <>
+      {/* Today's weather */}
+      <div className="mt-18">
+        <h1 className="p-10 text-5xl font-bold text-center">
+          How's the sky looking today?
+        </h1>
+
+        {/* Creating the search input and button */}
+        <div>
+          <input type="text" className="border" />
+        </div>
+      </div>
+    </>
+  );
 }
 
 export default SearchBar;
